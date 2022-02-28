2e7e52b775d66be90f29a9d98cf13f676eabb7f1
nicholastz <32432943+nicholastz@users.noreply.github.com>
2022-02-28 13:34:22 +0800

    Update diy-ssr-part1.sh

--- diy-ssr-part1.sh
+++ diy-ssr-part1.sh
@@ -15,5 +15,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git Smartdns https://github.com/pymumu/smartdns' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
